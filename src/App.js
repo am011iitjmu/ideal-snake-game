27375f136554ec64086ca534731046094fbb89f4
Anshuman Mishra <2019uch0019@iitjammu.ac.in>
2022-04-22 06:36:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Boards from './Components/Boards';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        Boards / >
+        <
+        /header> <
+        /div>
+    );
+}
+
+export default App;
